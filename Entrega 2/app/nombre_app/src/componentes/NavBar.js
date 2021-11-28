07dd793875099c441788a9ad03c03b153c70d82b
carlosrojaslara <carlosrojaslara@gmail.com>
2021-11-27 22:21:55 -0300

    entregaParcial

---
+++ Entrega 2/app/nombre_app/src/componentes/NavBar.js
@@ -0,0 +1,12 @@
+const Comp = ()=>{
+    return <header id="header">
+        <h1>EFECTOS DE SONIDO</h1>
+        <h2>tu mejor sonido</h2>
+        <nav id="link">
+            <a href="#">Personal</a>
+            <br></br>
+            <a href="#">Comercial</a>
+        </nav>
+    </header>
+}
+ export default Comp;
