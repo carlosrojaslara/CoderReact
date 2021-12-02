e17fa765f7e1f92bb1fc3d61c45c4e0b98ba244c
carlosrojaslara <carlosrojaslara@gmail.com>
2021-12-02 19:31:08 -0300

    parcial

---
+++ Entrega 4/app/src/components/ItemCount.js
@@ -0,0 +1,46 @@
+import {useState} from "react"
+
+const ItemCount = (props) => {
+
+    const [contador, setContador] = useState(props.valorInicial)
+
+    const aumentarContador = () => {
+
+        if (contador < props.stock)
+
+            setContador(contador + 1)
+    }
+
+    const disminuirContador = () =>{
+
+        if (  0 < contador )
+
+           setContador(contador - 1)
+
+    }
+
+    const onAdd = () =>{
+
+        alert("gracias por la compra")
+        
+    }
+
+
+        return (
+            <> <div>
+                    <button onClick={aumentarContador}>agregar</button>
+
+                    <p>cantidad de efectos comprados : {contador}</p>
+
+                    <button onClick={disminuirContador}>sacar</button>
+                </div>
+
+                <button onClick={onAdd}>Agregar al carrito</button> 
+            </>
+
+            )
+
+    }
+
+
+export default ItemCount;
---
+++ Entrega 4/app/src/components/ItemsListContainer.js
@@ -0,0 +1,15 @@
+import ItemCount from './ItemCount';
+
+const ItemListContainer =()=>{
+    const valorInicial =0
+    const stock=10
+
+    return <ItemCount valorInicial = {valorInicial} stock ={stock} />
+
+        
+        
+    
+
+}
+
+export default ItemListContainer;
