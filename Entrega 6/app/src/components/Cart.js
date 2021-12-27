3f049e18ae52c9ecfff5b6335f2f0692382eb9ba
carlosrojaslara <carlosrojaslara@gmail.com>
2021-12-27 00:45:35 -0300

    entrega11

--- Entrega 6/app/src/components/Cart.js
+++ Entrega 6/app/src/components/Cart.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import Context from './Context'
 import { useContext } from 'react'
+import { NavLink } from 'react-router-dom'; 
 
 
 const Cart = () => {
-    const { carrito } = useContext(Context)
+    const { carrito, handleDeleteItem } = useContext(Context)
     console.log(carrito.length);
 
     // const [precio, setPrecio] = useState('')
@@ -16,14 +17,21 @@ const Cart = () => {
         
     // }
 
+    const handleDelete = () =>{
+        handleDeleteItem();
+    }
 
     if (carrito.length === 1) {
   
         return (
+            <>
                 <div>
                     no hay items en el carrito
                 </div>
+                <NavLink to='/'>Comprar cosas</NavLink>
+            </>
             )
+           
         }
 
         else {
@@ -35,11 +43,13 @@ const Cart = () => {
                     <h2>{carrito[1].nombre}</h2>
                     <h3>{carrito[1].descripcion}</h3>
                     {/* <h4>precio total {() => precioTotal () }</h4> */}
+                    <button onClick={handleDelete}>eliminar item</button>
                 </div>
                 <div>
                     <h2>{carrito[1].nombre}</h2>
                     <h3>{carrito[1].descripcion}</h3>
                     <h4>{carrito[1].precio}</h4>
+                    <button onClick={handleDelete}>eliminar item</button>
                 </div>
 
                 </>
