1bb75f27ec447edac0a4c5858a88e8592ffd57fb
carlosrojaslara <carlosrojaslara@gmail.com>
2021-12-27 00:03:00 -0300

    entrega11

--- Entrega 6/app/src/components/Cart.js
+++ Entrega 6/app/src/components/Cart.js
@@ -1,13 +1,50 @@
+import { useState } from "react";
+import Context from './Context'
+import { useContext } from 'react'
 
 
 const Cart = () => {
-    //const resultado = useContext(context)
+    const { carrito } = useContext(Context)
+    console.log(carrito.length);
+
+    // const [precio, setPrecio] = useState('')
+
+    // const precioTotal = () => {
+    //     const total = carrito[1].precio * carrito[2];
+
+    //     return setPrecio(total)
+        
+    // }
+
+
+    if (carrito.length === 1) {
   
-    return (
-        <div>
-            cart
-        </div>
-    )
+        return (
+                <div>
+                    no hay items en el carrito
+                </div>
+            )
+        }
+
+        else {
+            
+            return (
+                <>
+                
+                <div>
+                    <h2>{carrito[1].nombre}</h2>
+                    <h3>{carrito[1].descripcion}</h3>
+                    {/* <h4>precio total {() => precioTotal () }</h4> */}
+                </div>
+                <div>
+                    <h2>{carrito[1].nombre}</h2>
+                    <h3>{carrito[1].descripcion}</h3>
+                    <h4>{carrito[1].precio}</h4>
+                </div>
+
+                </>
+            )
+    }
 }
 
 export default Cart;
